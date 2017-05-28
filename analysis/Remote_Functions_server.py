9d72c472c8a7231ed430f75aee10baec3c5612d3
Paul Gierz <pgierz@awi.de>
2017-05-28 16:05:31 +0200

    cleanup before sharing with group

--- analysis/Remote_Functions_server.py
+++
@@ -1,4 +0,0 @@
-import Pyro4
-
-from _analysis_helpers import Remote_Functions
-daemon = Pryo4.Daemon()
--- rpy2_PaleoLibrary/__init__.py
+++
@@ -1,7 +0,0 @@
-from rpy2.robjects.packages import importr
-
-base = importr('base')
-stats = importr('stats')
-
-# Import the paleolibrary?
-pallib = importr('../paleoLibrary/src/header.R')
